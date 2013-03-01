eef8b0a9ce46d0d977b32e9fd4b284e9d0d1760d
Seth W. Klein <sk@sethwklein.net>
2013-03-01 08:34:29 -0500

    Add error reporting

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func errMain() (err error) {
+	return nil
+}
+
 func main() {
+	err := errMain()
+	if err == nil {
+		os.Exit(0)
+	}
+	fmt.Fprintf(os.Stderr, "%v: Error: %v\n", filepath.Base(os.Args[0]), err)
+	os.Exit(1)
 }
